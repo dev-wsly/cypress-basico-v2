4d8670c1fab5f5a22b1362a41a2a3be1023d9e5f
Wesley Albino dos Santos <wesley.santos@mkdata.com.br>
2023-08-02 17:40:03 -0300

    Update

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -173,4 +173,26 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
         cy.get('.error').should('be.visible')
     })
+
+    //Seção 07 - Exercício
+    it('Seleciona um arquivo da pasta fixtures', function() {
+      cy.get('#file-upload')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json')
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    //Seção 07 - Exercício Extra 01
+    it('Seleciona um arquivo simulando um drag-and-drop', function() {
+      cy.get('#file-upload')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    //Seção 07 - Exercício Extra 02
 })
